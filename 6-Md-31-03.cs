6bba4c64af96153d6769f3ca15b8e9c615a33842
Bhawani Singh <128471145+bhawani-abjima@users.noreply.github.com>
2023-03-31 18:39:19 +0530

    Add files via upload

---
+++ 6-Md-31-03.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace test
+{
+    public class SimpleStack<T>
+    {
+        private readonly T[] _item;
+        private int _currentIndex = -1;
+        public SimpleStack() => _item = new T[10];
+        public int Count => _currentIndex + 1;
+        public void Push(T item) => _item[++_currentIndex] = item;
+        public T Pop() => _item[--_currentIndex];
+
+
+    }
+    class test
+    {
+        public static void Main(string[] args)
+        {
+            var stack = new SimpleStack<double>();
+            stack.Push(1.2);
+            stack.Push(2.8);
+            stack.Push(3.0);
+
+            double sum = 0.0;
+            while (stack.Count > 0)
+            {
+                double item = stack.Pop();
+                Console.WriteLine($"Item:{item}");
+                sum += item;
+
+            }
+            Console.WriteLine($"Sum:{sum}");
+            Console.ReadLine();
+        }
+    }
+}
